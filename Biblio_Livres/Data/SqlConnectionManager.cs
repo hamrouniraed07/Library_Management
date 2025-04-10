6a12de404622e063efe0b12936ef3601f4649128
mohamedazizsassi <mohamedaziz.sassi@polytechnicien.tn>
2025-04-10 19:08:10 +0100

    sqlConnectionManager

--- Biblio_Livres/Data/SqlConnectionManager.cs
+++ Biblio_Livres/Data/SqlConnectionManager.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace Biblio_Livres.Data
 {
-    class SqlConnectionManager
+    public static class SqlConnectionManager
     {
+        private static readonly string _connectionString =
+            "Data Source=LAPTOP-MIQGSNDC;Initial Catalog=Library;Integrated Security=True;Encrypt=False;Trust Server Certificate=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
     }
 }
