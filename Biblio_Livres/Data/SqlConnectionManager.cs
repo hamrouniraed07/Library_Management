60be9f21af0bc2e0befa92d674b00487a8cda3d4
HP <HP@DESKTOP-1L996PU>
2025-04-10 19:58:06 +0200

    Ajoutez des fichiers projet.

---
+++ Biblio_Livres/Data/SqlConnectionManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio_Livres.Data
+{
+    class SqlConnectionManager
+    {
+    }
+}
---
+++ Biblio_Livres/LibraryManager/Forms/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Biblio_Livres
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textUser = new TextBox();
+            textPass = new TextBox();
+            linkLabel1 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(522, 214);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(132, 24);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Show Password";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Ivory;
+            button1.Location = new Point(377, 294);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 52);
+            button1.TabIndex = 1;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ControlLight;
+            button2.Location = new Point(541, 294);
+            button2.Name = "button2";
+            button2.Size = new Size(113, 52);
+            button2.TabIndex = 2;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(287, 114);
+            label1.Name = "label1";
+            label1.Size = new Size(99, 28);
+            label1.TabIndex = 3;
+            label1.Text = "Username";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(293, 177);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 28);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            // 
+            // textUser
+            // 
+            textUser.Location = new Point(432, 118);
+            textUser.Name = "textUser";
+            textUser.Size = new Size(222, 27);
+            textUser.TabIndex = 5;
+            // 
+            // textPass
+            // 
+            textPass.Location = new Point(432, 181);
+            textPass.Name = "textPass";
+            textPass.PasswordChar = '*';
+            textPass.Size = new Size(222, 27);
+            textPass.TabIndex = 6;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = Color.Transparent;
+            linkLabel1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.ForeColor = Color.WhiteSmoke;
+            linkLabel1.LinkColor = Color.White;
+            linkLabel1.Location = new Point(489, 361);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(165, 28);
+            linkLabel1.TabIndex = 7;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Forget Password?";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(84, 115);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(159, 136);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.RosyBrown;
+            ClientSize = new Size(981, 476);
+            Controls.Add(pictureBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(textPass);
+            Controls.Add(textUser);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Name = "Form1";
+            Text = "Login Form";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private TextBox textUser;
+        private TextBox textPass;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox1;
+    }
+}
